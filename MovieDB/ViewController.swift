20c26b9222a6c9eed6819073ef9bfcea242f4224
Kecskés Péter <kecskes>
2021-08-15 15:36:01 +0200

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Kecskés Péter on 2021. 08. 15..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
